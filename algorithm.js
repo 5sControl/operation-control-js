2c1253947e1c8f3e0387d33c05075048eccab035
a-matyukh <amatyukh@gmail.com>
2023-06-22 21:07:38 +0300

    set default process.env

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,9 @@
 const Camera = require('./entities/Camera')
 const CornerCleaning = require("./entities/controls/CornerCleaning")
 
-const {camera_url, folder, server_url} = process.env
+const camera_url = process.env.camera_url || "http://192.168.1.110:3456/onvif-http/snapshot?Profile_1"
+const folder = process.env.folder || "images/192.168.1.110"
+const server_url = process.env.server_url || "http://192.168.1.110"
 
 const run = async () => {
     const camera = new Camera()
