3c58f5c5f9ce790ce5657bd37c217742e1d1560e
a-matyukh <amatyukh@gmail.com>
2023-06-22 18:33:47 +0300

    remove unused variables

--- algorithm.js
+++ algorithm.js
@@ -1,20 +1,13 @@
-const Camera = require('./entities/Camera');
-const CornerCleaning = require("./entities/controls/CornerCleaning");
+const Camera = require('./entities/Camera')
+const CornerCleaning = require("./entities/controls/CornerCleaning")
 
-
-const {camera_url, folder, server_url} = process.env;
+const {camera_url, folder, server_url} = process.env
 
 const run = async () => {
     const camera = new Camera()
     const reqBody = {algorithm: 'operation_control', camera_url, server_url, extra: []}
-    const SERVER_IP = server_url.split('//')[1]
     const cameraInterval = await camera.init(reqBody, false, folder)
     const createdAlgorithm = new CornerCleaning(cameraInterval.camera, 'operation_control', [])
-    const algorithmInterval = await createdAlgorithm.start(cameraInterval.camera)
+    await createdAlgorithm.start(cameraInterval.camera)
 }
-
 run()
-
-
-
-
