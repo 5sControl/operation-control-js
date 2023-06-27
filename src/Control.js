5c3913f87dc1d22e59bd592e064595a6c54624d0
a-matyukh <amatyukh@gmail.com>
2023-06-27 19:10:43 +0300

    extract modules to src-folder

--- src/Control.js
+++ src/Control.js
@@ -8,26 +8,26 @@ class Control {
     camera = new Camera()
     detector = new Detector()
     operation = new Operation()
-
+    
     async start(isWithTimer = true) {
         await this.detector.loadModels()
         if (isWithTimer) setInterval(() => this.check(), 1000)
         dispatcher.emit("control started")
     }
-
+    
     async check(bufferFromGer) {
         let checkBuffer = bufferFromGer ? bufferFromGer : await this.camera.getSnapshot()
         if (checkBuffer) {
             await this.detector.getPredictions(checkBuffer)
-
+    
             // this.detector.isWDetected()
             let detectClasses = new Set()
             for (const detection of this.detector.isAnyDetections()) detectClasses.add(detection.class)
-            // update this.window and this.worker
+            // update window and worker
             const window = this.detector.predictions.w?.find(detection => detection.class === "window" && detection.score > 0.5)
             let full_w = detectClasses.has("window") && detectClasses.has("worker")
             let empty_w = !detectClasses.has("window") && !detectClasses.has("worker")
-            // this.detector.isHKKDetected()
+            // detector.isHKKDetected()
             let isHKKdetected = this.detector.isAnyDetections(["hkk"], "boolean", 0.9)
     
             this.operation.check(checkBuffer, window, full_w, empty_w, isHKKdetected, this.detector.predictions.o[0])
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const dispatcher = require('./modules/Dispatcher')
+const dispatcher = require('./Dispatcher')
 dispatcher.emit("container started", `camera_url: ${process.env.camera_url}
 folder: ${process.env.folder}
 server_url: ${process.env.server_url}
 `)
 
-const {checkDirs} = require('./modules/utils/Path')
+const {checkDirs} = require('./utils/Path')
 checkDirs(["images", process.env.folder, "debug/", "debug/operation-control"])
 
-const Control = require("./modules/Control")
+const Control = require("./Control")
 new Control().start()
