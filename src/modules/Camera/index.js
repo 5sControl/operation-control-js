3346164f632e322e70079f346022367bcce3a278
a-matyukh <amatyukh@gmail.com>
2023-06-27 17:13:08 +0300

    reorganize Camera.snapshot

--- src/modules/Camera/index.js
+++ src/modules/Camera/index.js
@@ -1,40 +1,47 @@
 class Camera {
 
     snapshot = {
-        buffer: null
+        buffer: {
+            current: null,
+            previous: {
+                length: 0
+            },
+        },
+        saveLastLength() {
+            this.buffer.previous.length = this.buffer.current?.length
+        },
+        isExist() {
+            return this.buffer.current !== null
+        },
+        isAnother() {
+            return this.buffer.current.length === this.buffer.previous.length
+        },
+        isCorrect() {
+            return this.buffer.current.length > 300
+        }
     }
-
-    isLocalDebug = process.env.isLocalDebug || false
-    lastSnapshotLength = 0
 
     async getSnapshot() {
         try {
-            if (this.isLocalDebug) {
+            if (process.env.isLocalDebug) {
                 const fs = require('fs')
-                this.snapshot.buffer = fs.readFileSync('src/modules/Camera/snapshot.jpeg')
+                this.snapshot.buffer.current = fs.readFileSync('src/modules/Camera/snapshot.jpeg')
             } else {
                 const response = await fetch(process.env.camera_url)
                 console.log(response)
-                this.snapshot.buffer = response
+                this.snapshot.buffer.current = response
             }
-            if (!this.camera.snapshot.buffer) return
-            if (this.camera.snapshot.buffer.length < 1000) {
-                const {logger} = require("../Logger")
-                logger("translation broken", `buffer length is ${buffer.length} \n`)
-                return
-            }
-            if (this.camera.isVideoStreamOnPause()) return
+            if (!this.snapshot.isExist()) {console.log("snapshot null"); return}
+            if (!this.snapshot.isAnother()) {console.log("snapshot same"); return}
+            if (!this.snapshot.isCorrect()) {console.log("snapshot broken"); return}
+            this.snapshot.saveLastLength()
+            console.log("snapshot updated")
             return this.snapshot.buffer
         } catch (error) {
-            console.log('code: ', error.code)
-            console.log('socket: ', error.socket?.remoteAddress)
+            console.log("snapshot update error", error)
         }
     }
-    isVideoStreamOnPause() {
-        const isPause = this.snapshot.buffer.length === this.lastSnapshotLength
-        this.lastSnapshotLength = this.snapshot.buffer.length
-        return isPause
-    }
+
 }
 
 module.exports = Camera
