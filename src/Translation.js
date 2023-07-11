3b08ea8ac66e3668b29c6b96be4f2b29d60b811d
a-matyukh <amatyukh@gmail.com>
2023-07-11 08:46:02 +0300

    remove Translation.snapshot

--- src/Translation.js
+++ src/Translation.js
@@ -3,45 +3,52 @@ const fs = require('fs')
 
 class Translation {
 
-    snapshot = {
-        buffer: {
-            current: null,
-            previous: {
-                length: 0
-            },
-        },
-        saveLastLength() {
-            this.buffer.previous.length = this.buffer.current?.length
-        },
-        isExist() {
-            return this.buffer.current !== null
-        },
-        isAnother() {
-            return this.buffer.current.length !== this.buffer.previous.length
-        },
-        isCorrect() {
-            return this.buffer.current.length > 300000
+    buffer = {
+        current: null,
+        previous: {
+            length: 0
         }
     }
-    // snapshot/translation.update()
+    saveLastLength() {
+        this.buffer.previous.length = this.buffer.current?.length
+    }
+    isExist() {
+        return this.buffer.current !== null
+    }
+    isAnother() {
+        return this.buffer.current.length !== this.buffer.previous.length
+    }
+    isCorrect() {
+        return this.buffer.current.length > 300000
+    }
+
     async update(bufferFromGer) {
         try {
             if (bufferFromGer) {
-                this.snapshot.buffer.current = bufferFromGer
+                this.buffer.current = bufferFromGer
             } else {                
                 const response = await fetch(process.env.camera_url)
                 const arrayBuffer = await response.arrayBuffer()
-                this.snapshot.buffer.current = Buffer.from(arrayBuffer)
+                this.buffer.current = Buffer.from(arrayBuffer)
             }
 
             // snapshot.check()
-            if (!this.snapshot.isExist()) {dispatcher.emit("snapshot null"); return null}
-            if (!this.snapshot.isCorrect()) {dispatcher.emit("snapshot broken", `buffer length is ${this.snapshot.buffer.current.length} \n`); return null}
-            if (!this.snapshot.isAnother()) {dispatcher.emit("snapshot same"); return null}
+            if (!this.isExist()) {
+                dispatcher.emit("snapshot null"); 
+                return null
+            }
+            if (!this.isCorrect()) {
+                dispatcher.emit("snapshot broken", `buffer length is ${this.buffer.current.length} \n`); 
+                return null
+            }
+            if (!this.isAnother()) {
+                dispatcher.emit("snapshot same"); 
+                return null
+            }
 
-            this.snapshot.saveLastLength()
-            // this.recordSnapshot(this.snapshot.buffer.current)
-            dispatcher.emit("snapshot updated", false, this.snapshot.buffer.current)
+            this.saveLastLength()
+            // this.recordSnapshot(this.buffer.current)
+            dispatcher.emit("snapshot updated", false, this.buffer.current)
         } catch (error) {
             dispatcher.emit("snapshot update error", error)
         }
@@ -63,9 +70,7 @@ class Translation {
     constructor() {
         dispatcher.on("operation started", () => this.isRecording = true)
         dispatcher.on("operation finished", () => this.isRecording = false)
-        // this.type = "by_network"
-        this.type = "by_debugger"
-        if (this.type === "by_network") setInterval(() => this.update(), 1000)
+        if (!process.env.isDebugger) setInterval(() => this.update(), 1000)
     }
 
 }
