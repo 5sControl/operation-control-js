5af66c5435e485f53afe94d6d774fad8b76aec70
a-matyukh <amatyukh@gmail.com>
2023-07-11 10:26:23 +0300

    Detector.cutRegionFromBlob

--- src/Detector/index.js
+++ src/Detector/index.js
@@ -1,6 +1,6 @@
 const ModelWorker = require('./workers/ModelWorker')
-const Snapshot = require('../Snapshot')
 const {withinWorkspace} = require('../utils/2D')
+const {createCanvas, Image} = require('@napi-rs/canvas')
 
 class Detector {
 
@@ -26,8 +26,7 @@ class Detector {
         const detect_nothing = !worker_detection && !window_detection
         let action_detection
         if (worker_detection) {
-            const snapshot = new Snapshot(buffer)
-            const workerBlob = await snapshot.cutRegionFromBlob(worker_detection.bbox)
+            const workerBlob = await this.cutRegionFromBlob(buffer, worker_detection.bbox)
             let wo_detections = await this.model.o.exec(workerBlob)
             if (wo_detections[0]?.score > 0.9 && withinWorkspace(wo_detections[0]?.bbox, [1600, 900])) {
                 wo_detections = wo_detections.map(d => {
@@ -48,6 +47,28 @@ class Detector {
             action_detection // undefined || YoloDetection
         }
         return this.detections
+    }
+    /**
+     * @returns {Blob}
+     */
+    async cutRegionFromBlob(buffer, region) {
+    
+        const [cHeight, cWidth] = [1080, 1920]
+        let canvas = createCanvas(cWidth, cHeight)
+        let ctx = canvas.getContext('2d')
+        const image = new Image()
+        image.src = buffer
+        ctx.drawImage(image, 0, 0)
+
+        const [x, y, width, height] = region
+        const OFFSET = 20
+        let cuttedWorker = ctx.getImageData(x - OFFSET, y - OFFSET, width + OFFSET, height + OFFSET)
+
+        let newCan = createCanvas(width + OFFSET, height + OFFSET)
+        let newCtx = newCan.getContext('2d')
+        newCtx.putImageData(cuttedWorker, 0, 0)
+        const croppedBlob = await newCan.encode('jpeg', 90)
+        return croppedBlob
     }
     async detectBatch(batch) {
         const prev = Date.now()
--- src/Snapshot.js
+++ src/Snapshot.js
@@ -95,29 +95,6 @@ class Snapshot {
         await Promise.all(promises)
         this.buffer = await this.canvas.encode('jpeg', 50)
     }
-
-    /**
-     * @returns {Blob}
-     */
-    async cutRegionFromBlob(region) {
-        
-        const [cHeight, cWidth] = [1080, 1920]
-        let canvas = createCanvas(cWidth, cHeight)
-        let ctx = canvas.getContext('2d')
-        const image = new Image()
-        image.src = this.buffer
-        ctx.drawImage(image, 0, 0)
-
-        const [x, y, width, height] = region
-        const OFFSET = 20
-        let cuttedWorker = ctx.getImageData(x - OFFSET, y - OFFSET, width + OFFSET, height + OFFSET)
-
-        let newCan = createCanvas(width + OFFSET, height + OFFSET)
-        let newCtx = newCan.getContext('2d')
-        newCtx.putImageData(cuttedWorker, 0, 0)
-        const croppedBlob = await newCan.encode('jpeg', 90)
-        return croppedBlob
-    }
     
 }
 
