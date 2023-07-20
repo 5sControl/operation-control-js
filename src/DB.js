45621f25e5a63beb52d32e240062f676bbbe42ac
a-matyukh <amatyukh@gmail.com>
2023-07-20 08:13:09 +0300

    trycatch db operations

--- src/DB.js
+++ src/DB.js
@@ -2,19 +2,27 @@ const { supabase } = require("./supabaseClient.js")
 
 const db = {
     async insert(table, record) {
-        const { data, error } = await supabase
-        .from(table)
-        .insert([record])
-        .select()
-        console.log("record was inserted to:", table, "record:", data, "error:", error)
+        try {
+            const { data, error } = await supabase
+            .from(table)
+            .insert([record])
+            .select()
+            console.log("record was inserted to:", table, "record:", data, "error:", error)
+        } catch (error) {
+            console.log(error)
+        }
     },
     async upload(snapshot) {
-        const { data, error } = await supabase.storage
-        .from("snapshots").upload(`${snapshot.launch}/${snapshot.index}.jpeg`, snapshot.compressed_buffer, {upsert: true})
-        if (error) {
+        try {
+            const { data, error } = await supabase.storage
+            .from("snapshots").upload(`${snapshot.launch}/${snapshot.index}.jpeg`, snapshot.compressed_buffer, {upsert: true})
+            if (error) {
+                console.log(error)
+            } else {
+                console.log("snapshot buffer saved", data)
+            }            
+        } catch (error) {
             console.log(error)
-        } else {
-            console.log("snapshot buffer saved", data)
         }
     }
 }
